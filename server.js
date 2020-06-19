e4432e6d9d842119b039515325a863b967f4aa93
PaRiBuS <oiram.araujo@gmail.com>
2020-06-19 00:55:47 -0300

    Code for Heroku

--- server.js
+++ server.js
@@ -49,6 +49,16 @@ app.get('/api/users/:username/details', (req, res) => {
         catch(err => res.status(500).json({ message: err.message }));
 });
 
+if(process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+        
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
