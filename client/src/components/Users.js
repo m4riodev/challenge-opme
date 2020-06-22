e8fbbe198cf4d29b5784369043f0ee8a9a90340f
PaRiBuS <oiram.araujo@gmail.com>
2020-06-22 00:12:31 -0300

    Little fix

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams, useHistory, useRef } from 'react-router-dom';
+import React, { useEffect, useState, useRef } from 'react';
+import { Link, useParams, useHistory } from 'react-router-dom';
 import { 
   Segment, 
   Dimmer, 
